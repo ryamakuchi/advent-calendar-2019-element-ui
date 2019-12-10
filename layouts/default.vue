f8a8d4ddad658fd74aadc4777b1ca98e04babb13
rry <r.yamakuchi@gmail.com>
2019-12-10 23:37:45 +0900

    add: header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,53 @@
 <template>
   <div>
+    <el-menu default-active="1" mode="horizontal">
+      <el-menu-item index="1">
+        <logo />
+
+        <el-link
+          type="primary"
+          :underline="false"
+          href="https://qiita.com/advent-calendar/2019/nuxt-js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="brand"
+        >
+          Nuxt.js Advent Calendar 2019 for Qiita
+        </el-link>
+      </el-menu-item>
+
+      <el-menu-item index="2" class="float-right">
+        <el-link
+          href="https://element.eleme.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made with Element
+        </el-link>
+      </el-menu-item>
+    </el-menu>
+
     <nuxt />
   </div>
 </template>
 
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+.el-menu .brand {
+  font-size: 18px;
+  padding-left: 10px;
+  color: #409EFF !important;
 }
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.el-menu .float-right {
+  float: right;
 }
 </style>
